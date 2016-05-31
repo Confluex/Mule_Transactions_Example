aee0bde19dffe13f38ee8f39a7862913ee9d2498
Paul Smith <paul0731@gmail.com>
2016-05-31 10:11:43 -0500

    Add foreign key, change db to start/stop between tests, get rid of reset tables.

--- src/main/resources/jdbc/ddl.sql
+++ src/main/resources/jdbc/ddl.sql
@@ -13,5 +13,16 @@ CREATE TABLE IF NOT EXISTS salary_history(
       salary INT NOT NULL,
       from_date DATE NOT NULL,
       to_date DATE NOT NULL,
-      PRIMARY KEY (emp_no,from_date)
+      PRIMARY KEY (emp_no,from_date),
+      FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
+
+INSERT INTO employees(emp_no,birth_date,first_name,last_name,gender,hire_date) 
+      VALUES (10001, '1973-09-02', 'George', 'Johnson', 'M', '2012-06-26');
+
+      INSERT INTO salary_history(emp_no,salary,from_date,to_date)
+      VALUES(10001,85112,'2012-06-26','2014-06-22');
+INSERT INTO salary_history(emp_no,salary,from_date,to_date)
+      VALUES(10001,85097,'2014-06-22','2015-06-22');
+INSERT INTO salary_history(emp_no,salary,from_date,to_date)
+      VALUES(10001,88958,'2015-06-22','9999-01-01');
